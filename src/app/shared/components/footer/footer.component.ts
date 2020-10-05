f180e07bea7f75654576bb53c9ecbc819cb76016
filipbarak94@gmail.com <filipbarak94@gmail.com>
2020-10-05 21:18:37 +0200

    add initial styles

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -6,8 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
+  readonly currentYear = new Date().getFullYear();
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -13,11 +13,15 @@ import { LoadingComponent } from './components/loading/loading.component';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatCardModule } from '@angular/material/card';
 import { NgApexchartsModule } from 'ng-apexcharts';
+import { MatIconModule } from '@angular/material/icon';
+
 
 const components = [FooterComponent, HeaderComponent, SidebarComponent, NotificationComponent, LoadingComponent];
 const directives = [];
 const pipes = [FilterPipe];
-const modules = [MatSliderModule, MatSnackBarModule, MatButtonModule, MatProgressSpinnerModule, MatCardModule, NgApexchartsModule,];
+const modules = [
+  MatSliderModule, MatSnackBarModule, MatButtonModule, MatProgressSpinnerModule, MatCardModule, NgApexchartsModule, MatIconModule
+];
 
 @NgModule({
   declarations: [...components, ...directives, ...pipes],
