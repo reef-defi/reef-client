ef270d92d7d8f464c0c3ae05cfb5025a271fe5b3
filipbarak94@gmail.com <filipbarak94@gmail.com>
2020-10-23 16:42:41 +0200

    minor pool bugfix

--- src/app/core/services/contract.service.ts
+++ src/app/core/services/contract.service.ts
@@ -43,6 +43,7 @@ export class ContractService {
       })));
       baskets = getBasketPoolNames(baskets, this.apiService.pools$.value, this.apiService.tokens$.value)
         .filter(basket => +basket.investedETH > 0);
+      console.log(baskets, 'wtf??')
       this.baskets$.next(baskets);
       this.basketsError$.next(false);
       this.loading$.next(false);
