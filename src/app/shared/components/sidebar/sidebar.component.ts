e6b6c56d0628642a5d2a9c985c217cff763008ec
matjazhi <matjaz.hirsman@better.care>
2021-01-05 19:13:37 +0100

    Burger menu navigation Closes #47

--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -1,7 +1,7 @@
-import { Component, EventEmitter, Input, Output, TemplateRef, ViewChild } from '@angular/core';
-import { IChainData } from '../../../core/models/types';
-import { MatDialog } from '@angular/material/dialog';
-import { UniswapService } from '../../../core/services/uniswap.service';
+import {Component, EventEmitter, HostListener, Input, OnInit, Output} from '@angular/core';
+import {IChainData} from '../../../core/models/types';
+import {MatDialog} from '@angular/material/dialog';
+import {UniswapService} from '../../../core/services/uniswap.service';
 import {ConnectorService} from "../../../core/services/connector.service";
 
 @Component({
@@ -9,16 +9,31 @@ import {ConnectorService} from "../../../core/services/connector.service";
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.scss']
 })
-export class SidebarComponent {
+export class SidebarComponent implements OnInit {
   readonly currentYear = new Date().getFullYear();
   @Input() chainInfo: IChainData | undefined;
   @Input() version: string | undefined;
   @Input() currentAddress: string | undefined;
   @Output() signOut = new EventEmitter();
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+    this.innerWidth = window.innerWidth;
+  }
+
   readonly pendingTx$ = this.connectorService.pendingTransaction$;
+  sidebarOpen: boolean;
+  innerWidth: number;
+  manualToggle: boolean;
+  minWidth = 992;
 
   constructor(private dialog: MatDialog, private readonly uniswapService: UniswapService,
-              private readonly connectorService: ConnectorService) {}
+              private readonly connectorService: ConnectorService) {
+  }
+
+  ngOnInit(): void {
+    this.innerWidth = window.innerWidth;
+  }
 
   onSignOut(): void {
     this.signOut.emit();
