802156830098b81009e85554f3fc5010c0828ce3
mhirsman <matjazonline>
2021-01-19 23:27:16 +0100

    Add analytics/remarketing tags

--- src/app/modules/reef/pages/card/card.page.ts
+++ src/app/modules/reef/pages/card/card.page.ts
@@ -7,6 +7,7 @@ import {switchMap} from 'rxjs/internal/operators/switchMap';
 import {startWith} from 'rxjs/internal/operators/startWith';
 import {catchError, shareReplay, takeUntil} from 'rxjs/operators';
 import {of} from 'rxjs/internal/observable/of';
+import {GoogleAnalyticsService} from '../../../../shared/service/google-analytics.service';
 
 interface StatusInfo {
   notInterested?: string;
@@ -24,17 +25,18 @@ interface StatusInfo {
 export class CardPage implements OnDestroy {
 
   statusInfo: Observable<StatusInfo>;
-  login_form: FormGroup;
+  loginForm: FormGroup;
   statusSub = new Subject<string>();
   destroyed = new Subject<void>();
 
-  constructor(private http: HttpClient, fb: FormBuilder) {
-    this.login_form = fb.group({
+  constructor(private http: HttpClient, fb: FormBuilder,
+              private googleAnalyticsService: GoogleAnalyticsService) {
+    this.loginForm = fb.group({
       email: [null, Validators.compose([Validators.required, Validators.pattern(/^(\d{10}|\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3}))$/)])],
     });
 
     this.statusInfo = this.statusSub.pipe(
-      switchMap(status => status.email ? this.http.post(environment.reefNodeApiUrl + '/card-interest', {email: status.email}) : of(status)),
+      switchMap((status: any) => status.email ? this.http.post(environment.reefNodeApiUrl + '/card-interest', {email: status.email}) : of(status)),
       startWith({notInterested: true, message: null}),
       catchError(err => of({message: 'There was a problem sending data. Please try later.', error: true})),
       shareReplay(1)
@@ -45,10 +47,10 @@ export class CardPage implements OnDestroy {
     ).subscribe((info: StatusInfo) => {
       if (info.message === null && !info.notInterested) {
         // interested
+        googleAnalyticsService.eventEmitter('view_item', 'engagement', 'cart_interest_open');
       } else if (info.email) {
         // ga. emailSent
-      } else if (!!info.message && !!info.error) {
-        // error with sending
+        googleAnalyticsService.eventEmitter('sign_up', 'engagement', 'cart_interest_sent');
       }
     });
     // page opened
---
+++ src/app/shared/service/google-analytics.service.ts
@@ -0,0 +1,29 @@
+import {Injectable} from "@angular/core";
+
+declare let gtag: Function;
+
+@Injectable({providedIn: 'root'})
+export class GoogleAnalyticsService {
+
+  pageView(uri: string) {
+    gtag('config', 'G-G1W8EPNCTC', {
+      page_path: uri
+    });
+  }
+
+  eventEmitter(
+    eventName: string,
+    eventCategory: string,
+    eventAction: string,
+    eventLabel: string = null,
+    eventValue: number = null) {
+    console.log('analytics event=', eventLabel);
+    gtag('event', eventName, {
+      eventCategory: eventCategory,
+      eventLabel: eventLabel,
+      eventAction: eventAction,
+      eventValue: eventValue
+    });
+  }
+
+}
