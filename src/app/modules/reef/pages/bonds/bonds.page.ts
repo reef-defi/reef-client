068b9f6513049e76dfa32da14703ab2fad1fd296
mhirsman <matjazonline>
2021-02-09 13:29:08 +0100

    Bonds - UI updates

--- src/app/modules/reef/pages/bonds/bonds.page.ts
+++ src/app/modules/reef/pages/bonds/bonds.page.ts
@@ -1,12 +1,16 @@
-import {Component} from '@angular/core';
+import {ChangeDetectionStrategy, Component} from '@angular/core';
 import {BondsService} from '../../../../core/services/bonds.service';
+import {UiUtils} from '../../../../shared/utils/ui.utils';
 
 @Component({
   selector: 'app-bonds',
   templateUrl: './bonds.page.html',
   styleUrls: ['./bonds.page.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class BondsPage {
+  UiUtils = UiUtils;
+
   constructor(public bondsService: BondsService) {
   }
 }
