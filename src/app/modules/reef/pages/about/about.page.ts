a6cd9458559d53c43a533f6723b8d45d1486f070
unknown <filipbarak94@gmail.com>
2021-02-11 16:42:41 +0100

    Change homepage, update walkthrough

--- src/app/modules/reef/pages/about/about.page.ts
+++ src/app/modules/reef/pages/about/about.page.ts
@@ -1,13 +1,10 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-about-page',
   templateUrl: './about.page.html',
-  styleUrls: ['./about.page.scss']
+  styleUrls: ['./about.page.scss'],
 })
 export class AboutPage {
-
-  constructor() {
-  }
-
+  constructor() {}
 }
