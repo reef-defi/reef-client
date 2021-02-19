d91647dd4c1404127be04524c761c75fbad8b255
matjazonline <matjazonline>
2021-02-19 12:26:33 +0100

    Development only logs

---
+++ src/app/shared/utils/dev-util.ts
@@ -0,0 +1,26 @@
+import {environment} from '../../../environments/environment';
+
+export class DevUtil {
+  static devLog(...args): void {
+    let logAtLevel = LogLevel.STD;
+    if (args.length && args[args.length - 1].logLevel != null) {
+      logAtLevel = args[args.length - 1].logLevel;
+      args.splice(args.length - 1, 1);
+    }
+    if (environment.logLevel >= logAtLevel) {
+      if (logAtLevel > 0) {
+        console.log.call(null, ...args);
+      } else if (logAtLevel === LogLevel.WARNING) {
+        console.warn.call(null, ...args);
+      } else if (logAtLevel === LogLevel.ERROR) {
+        console.error.call(null, ...args);
+      }
+    }
+  }
+}
+
+export enum LogLevel {
+  ERROR = -1,
+  WARNING = 0,
+  STD = 1
+}
